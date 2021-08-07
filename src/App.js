c21a9491cb16a0e35f4425ddea363c0eb5f0bf54
Daniel <itzdanielmichael@gmail.com>
2021-08-06 21:34:32 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,45 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 function App() {
   const API_URL = (numberOfWords) => `https://random-word-api.herokuapp.com/word?number=${numberOfWords}`
-  const [words, setWords] = useState([])
+  const [words, setWords] = useState([]);
+  const [error, setError] = useState(false);
+  const [numbers, setNumbers] = useState('')
   const [ign, setIgn] = useState('');
 
   useEffect(() => {
-    axios.get(API_URL(1000)).then(({data}) => setWords(data))
+    axios.get(API_URL(1000)).then(({ data }) => setWords(data)).catch((_err) => setError(true));
   }, [])
   
-  const handleCreateIgn = () => {
-    setIgn(prev => prev)
+  const handleCreateIgn = (e) => {
+    e.preventDefault();
+    const randomWordOne = sample(words);
+    const randomWordTwo = sample(words);
+
+    const resultWord = (randomWordOne + randomWordTwo).toUpperCase()
+    console.log({resultWord})
+    setIgn(resultWord + numbers)
   }
 
   return (
     <div className="App">
-      {words.map((w) => <h1>{w}</h1>)}
+      <h1>tyler1 ign generator</h1>
+      {ign ? <h2>IGN: {ign}</h2> : null}
 
-      <button onClick={handleCreateIgn}>Generate IGN</button>
+      {!error ? <form onSubmit={handleCreateIgn}>
+        <div>
+         
+          <input
+            placeholder="enter a number"
+            type="number" onChange={(e) => setNumbers(String(e.target.value))}
+          />
+        </div>
+        <br />
+        <button>Generate IGN</button>
+      </form> : <>sorry! something went wrong.</>}
     </div>
   );
 }
