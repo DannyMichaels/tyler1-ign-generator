ad44706a0ddc65d42f05012d137078e294430058
Daniel <itzdanielmichael@gmail.com>
2021-08-06 21:58:41 -0400

    upd main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import Api from './api';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
@@ -34,26 +36,32 @@ function App() {
 
   return (
     <div className="App">
-      <h1>tyler1 ign generator</h1>
-      {ign ? <h2>IGN: {ign}</h2> : null}
+      <div className="centered">
+        <h1>tyler1 ign generator</h1>
+        {ign ? <h2>IGN: {ign}</h2> : null}
 
-      {!error ? (
-        <form onSubmit={handleCreateIgn}>
-          <div>
-            <input
-              placeholder="enter a number"
-              type="number"
-              required
-              value={numbers}
-              onChange={(e) => setNumbers(String(e.target.value))}
-            />
-          </div>
-          <br />
-          <button>Generate IGN</button>
-        </form>
-      ) : (
-        <h1>sorry! something went wrong.</h1>
-      )}
+        {!error ? (
+          <form onSubmit={handleCreateIgn}>
+            <div>
+              <TextField
+                variant="filled"
+                style={{ background: '#fff' }}
+                placeholder="enter a number"
+                type="number"
+                required
+                value={numbers}
+                onChange={(e) => setNumbers(String(e.target.value))}
+              />
+            </div>
+            <br />
+            <Button type="submit" variant="contained" color="primary">
+              Generate IGN
+            </Button>
+          </form>
+        ) : (
+          <h1>sorry! something went wrong.</h1>
+        )}
+      </div>
     </div>
   );
 }
