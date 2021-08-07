ca442effe8ecefd702d9598279b2178a3d6a8fc8
Daniel <itzdanielmichael@gmail.com>
2021-08-07 19:59:28 -0400

    update MyList.jsx

--- src/components/MyList.jsx
+++ src/components/MyList.jsx
@@ -1,4 +1,4 @@
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/DeleteForever';
 
 export default function MyList({ showBackground, listItems, removeFromList }) {
