87e71cabe0df9fd23e141401f75aac460340a241
Daniel <itzdanielmichael@gmail.com>
2021-08-07 13:11:53 -0400

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+const BACKGROUND_IMG = 'https://i.redd.it/9rlwdb2br9jz.png';
+
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+};
 
 function App() {
   const [words, setWords] = useState([]);
@@ -20,6 +22,7 @@ function App() {
   const [enterNumsForMe, setEnterNumsForMe] = useState(true);
   const [ign, setIgn] = useState('');
   const [visitedRandomWords, setVisitedRandomWords] = useState(new Set()); // get unique generated IGNS to avoid boredom
+  const [showBackground, setShowBackground] = useState(true);
 
   useEffect(() => {
     const api = new Api();
@@ -72,7 +75,12 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: showBackground ? `url(${BACKGROUND_IMG})` : '',
+        minHeight: '100vh',
+      }}>
       <div className="centered">
         <h1>tyler1 ign generator</h1>
         {ign ? <h2>IGN: {ign}</h2> : null}
@@ -134,9 +142,18 @@ function App() {
         <a
           target="_blank"
           rel="noreferrer"
+          style={!showBackground ? { color: 'black' } : {}}
           href="https://github.com/DannyMichaels/tyler1-ign-generator">
           source code
         </a>
+
+        <Button
+          className="toggle-bg-btn"
+          color={showBackground ? 'secondary' : 'primary'}
+          variant="contained"
+          onClick={() => setShowBackground((prev) => !prev)}>
+          {showBackground ? 'Disable background' : 'Enable background'}
+        </Button>
       </footer>
     </div>
   );
